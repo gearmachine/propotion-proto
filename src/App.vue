efd0521d9304b9098f2cf8860e995d502f16c7d6
shin.sgym <web@kudaranai.jp>
2021-04-15 20:41:18 +0900

    ver 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <svg version="1.1"
+     baseProfile="full"
+     :width="width" :height="height"
+     xmlns="http://www.w3.org/2000/svg">
+
+      <rect :x="x" :y="y" :width="w" :height="h" stroke="black" fill="white" stroke-width="5" />
+      
+    </svg>
+    <button v-on:click="changeProportion">変更</button>
+    <div>(x, y) = ({{ x + ', ' + y }})</div>
+    <div>(w, h) = ({{ w + ', ' + h }}) &nbsp;&nbsp; {{ calcProportion() }}</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      width: 800,
+      height: 600,
+      x: 10,
+      y: 10,
+      w: 150,
+      h: 150,
+    }
+  },
+  created() {
+    this.changeProportion();
+  },
+  methods: {
+    changeProportion: function() {
+      const max = 7;
+      const min = 1;
+      do {
+        this.x = Math.floor(this.width * Math.random());
+        this.y = Math.floor(this.height * Math.random());
+        this.w = 50 * (Math.floor( 2 * (Math.random() * (max + 1 - min)) + min));
+        this.h = 50 * (Math.floor( 2 * (Math.random() * (max + 1 - min)) + min));
+      } while ( this.x > this.width * 0.7 
+        || this.y > this.height * 0.7 
+        || this.x + this.w > this.width
+        || this.y + this.h > this.height
+        || this.w < 100
+        || this.h < 100
+        || this.w * 4 < this.h
+        || this.w > this.h * 4
+        || this.w == this.h
+      )
+    },
+    calcProportion: function() {
+      let gcd = this.gcd(this.w, this.h);
+      return '' + this.w / gcd + ':' + this.h / gcd + ' = '
+       + ((this.w > this.h) ? '' + Math.round(this.w/this.h * 100) / 100 + ':1' : '1:' + Math.round(this.h / this.w * 100) / 100);
+    },
+    gcd: function(m, n) {
+      // 最大公約数を求める
+      if (n == 0) return m;
+      return this.gcd(n, m % n);
+    }
   }
 }
 </script>
